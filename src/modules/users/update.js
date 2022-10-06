abdc94e06324ed3d3ed400aca54cfaa1fee8ddb1
imamramzani <imam.ramzani@nadihealth.com>
2022-10-06 18:47:42 +0700

    handle error

--- src/modules/users/update.js
+++ src/modules/users/update.js
@@ -8,10 +8,10 @@ const bodyRules = joi.object({
     identityNumber: joi.string(),
     accountNumber: joi.string()
 })
-module.exports = async function (req, res){
+module.exports = async function (req, res) {
     const col = req.mongoDB.db(req.mainDB).collection("users")
     const params = req.params
-    let body = await bodyValidation(req,res)
+    let body = await bodyValidation(req, res)
 
     const objToUpdate = {
         ...body,
@@ -19,20 +19,20 @@ module.exports = async function (req, res){
     }
 
     try {
-        const item = await col.findOne({_id: params.id})
-        if(!item) return res.status(401).json({
+        const item = await col.findOne({ _id: params.id })
+        if (!item) return res.status(401).json({
             msg: "Can not find user"
         })
 
-        let updated = await col.updateOne({_id: params.id}, {$set: objToUpdate})
-        updated = await col.findOne({_id: params.id}, { projection: { _id: 1, userName: 1, accountNumber: 1, emailAddress: 1, identityNumber: 1, createdAt: 1 } })
+        let updated = await col.updateOne({ _id: params.id }, { $set: objToUpdate })
+        updated = await col.findOne({ _id: params.id }, { projection: { _id: 1, userName: 1, accountNumber: 1, emailAddress: 1, identityNumber: 1, createdAt: 1 } })
         await redis.del('AllUser')
         return res.status(200).json({
             data: updated,
             msg: "Success update data"
         })
     } catch (err) {
-        if(err.code){
+        if (err.code) {
             return res.status(err.code).json({
                 code: err.code,
                 success: err.success,
